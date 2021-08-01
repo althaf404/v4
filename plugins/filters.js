39b5494e51c0b53dc293c3df5e3f14da6733975f
althaf kp <84726750+althaf404@users.noreply.github.com>
2021-08-01 16:24:05 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -140,3 +140,234 @@ filtreler.map(
     }
 );
 }));
+
+
+Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+    match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
+
+    if (match === null) {
+
+        filtreler = await FilterDb.getFilter(message.jid);
+
+        if (filtreler === false) {
+
+            await message.client.sendMessage(message.jid,Lang.NO_FILTER,MessageType.text)
+
+        } else {
+
+            var mesaj = Lang.FILTERS + '\n';
+
+            filtreler.map((filter) => mesaj += '```' + filter.dataValues.pattern + '```\n');
+
+            await message.client.sendMessage(message.jid,mesaj,MessageType.text);
+
+        }
+
+    } else {
+
+        if (match.length < 2) {
+
+            return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + ' ```.filter "sa" "as"',MessageType.text);
+
+        }
+
+        await FilterDb.setFilter(message.jid, match[0].replace(/['"“]+/g, ''), match[1].replace(/['"“]+/g, '').replace(/[#]+/g, '\n'), match[0][0] === "'" ? true : false);
+
+        await message.client.sendMessage(message.jid,Lang.FILTERED.format(match[0].replace(/['"]+/g, '')),MessageType.text);
+
+    }
+
+}));
+
+Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+    match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
+
+    if (match === null) {
+
+        return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
+
+    }
+
+    del = await FilterDb.deleteFilter(message.jid, match[0].replace(/['"“]+/g, ''));
+
+    
+
+    if (!del) {
+
+        await message.client.sendMessage(message.jid,Lang.ALREADY_NO_FILTER, MessageType.text)
+
+    } else {
+
+        await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)
+
+    }
+
+}));
+
+Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+
+    if(Config.BGMFILTER){
+
+    let banned = jid.find( Jid => Jid === message.jid);
+
+    if(banned !== undefined) return
+
+    if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
+
+await message.client.sendMessage(message.jid, fs.readFileSync('./videos/mention.mp4'), MessageType.videos, { mimetype: Mimetype.mp4, quoted : message.data, ptt: false})
+
+    }
+
+const array = ['welcome']
+
+array.map( async (a) => {
+
+let pattern = new RegExp(`\\b${a}\\b`, 'g');
+
+if(pattern.test(message.message)){
+
+   await message.client.sendMessage(message.jid, fs.readFileSync('./videos/' + a + '.mp4'), MessageType.videos, { mimetype: Mimetype.mp4, quoted: message.data, ptt: false})
+
+}
+
+});Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+    match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
+
+    if (match === null) {
+
+        filtreler = await FilterDb.getFilter(message.jid);
+
+        if (filtreler === false) {
+
+            await message.client.sendMessage(message.jid,Lang.NO_FILTER,MessageType.text)
+
+        } else {
+
+            var mesaj = Lang.FILTERS + '\n';
+
+            filtreler.map((filter) => mesaj += '```' + filter.dataValues.pattern + '```\n');
+
+            await message.client.sendMessage(message.jid,mesaj,MessageType.text);
+
+        }
+
+    } else {
+
+        if (match.length < 2) {
+
+            return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + ' ```.filter "sa" "as"',MessageType.text);
+
+        }
+
+        await FilterDb.setFilter(message.jid, match[0].replace(/['"“]+/g, ''), match[1].replace(/['"“]+/g, '').replace(/[#]+/g, '\n'), match[0][0] === "'" ? true : false);
+
+        await message.client.sendMessage(message.jid,Lang.FILTERED.format(match[0].replace(/['"]+/g, '')),MessageType.text);
+
+    }
+
+}));
+
+Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+    match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
+
+    if (match === null) {
+
+        return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
+
+    }
+
+    del = await FilterDb.deleteFilter(message.jid, match[0].replace(/['"“]+/g, ''));
+
+    
+
+    if (!del) {
+
+        await message.client.sendMessage(message.jid,Lang.ALREADY_NO_FILTER, MessageType.text)
+
+    } else {
+
+        await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)
+
+    }
+
+}));
+
+Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+
+    if(Config.BGMFILTER){
+
+    let banned = jid.find( Jid => Jid === message.jid);
+
+    if(banned !== undefined) return
+
+    if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
+
+await message.client.sendMessage(message.jid, fs.readFileSync('./videos/mention.mp4'), MessageType.video, { mimetype: Mimetype.mp4, quoted : message.data, ptt: false})
+
+    }
+
+const array = ['welcome']
+
+array.map( async (a) => {
+
+let pattern = new RegExp(`\\b${a}\\b`, 'g');
+
+if(pattern.test(message.message)){
+
+   await message.client.sendMessage(message.jid, fs.readFileSync('./videos/' + a + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4, quoted: message.data, ptt: false})
+
+}
+
+});
+
+}
+
+var filtreler = await FilterDb.getFilter(message.jid);
+
+if (!filtreler) return; 
+
+filtreler.map(
+
+    async (filter) => {
+
+        pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
+
+        if (pattern.test(message.message)) {
+
+            await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
+
+        }
+
+    }
+
+);
+
+}));
+
+}
+
+var filtreler = await FilterDb.getFilter(message.jid);
+
+if (!filtreler) return; 
+
+filtreler.map(
+
+    async (filter) => {
+
+        pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
+
+        if (pattern.test(message.message)) {
+
+            await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
+
+        }
+
+    }
+
+);
+
+}));
